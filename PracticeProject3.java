f4756167e83988586f4bb8fd29142561e9878e3d
qustandifashho <75642117+qustandifashho@users.noreply.github.com>
2023-04-30 23:02:31 -0500

    Add files via upload

--- PracticeProject3.java
+++ PracticeProject3.java
@@ -5,6 +5,7 @@ public class PracticeProject3{
   public static final Scanner CONSOLE = new Scanner(System.in); // Global variable
   public static void main(String[] args) {
     System.out.println("UTSA – Fall 2022 - CS1083 - Section 001 - Project 3 - SyncedForms - written by Qustandi Fashho");
+    System.out.println();
     drawSyncedForms();                                                                                                   
   }
       public static void drawSyncedForms() {
@@ -12,17 +13,48 @@ public class PracticeProject3{
         Graphics g = panel.getGraphics();
         
         // declare and initialize arrays
-        int[] xPositions = new int[4];
-        int[] yPositions = new int[4];
-        char[] forms = new char[]{'R', 'R', 'C', 'C'};
-        Color[] colors = new Color[4];
-        int numForms = 4;
-        int numMoves = 5;
+        int[] xPositions = new int[9];
+        int[] yPositions = new int[9];
+        char[] forms = new char[]{'S','s', 'R', 'r'};
+        Color[] colors = new Color[9];
+        int numForms = 4; // 
+        int numMoves = 5; //
+        int xPos = 0;
+        int yPos = 0;
+        int size = 0;
+        
+        boolean isValidInput = true;
+        while(isValidInput){
+          System.out.print("What form will be shown (C-ircle, R-ectangle)?"); ////////////////////
+          char form  = CONSOLE.next().charAt(0); /////////////////////
+          if (form == 'S' || form == 's' || form == 'R' || form == 'r') {
+            xPos = 0;
+            yPos = 0;
+            size = 100;
+            //g.fillRect(xPos, yPos, size, size / 2); //////////////// 
+            break;
+        } else if (form == 'C' || form == 'c') {
+            xPos = 0;
+            yPos = 0;
+            size = 100;
+            //g.drawOval(xPos, yPos, size, size);
+            break;
+        } else {
+            System.out.println("Invalid form input: " + form);
+            
+        }
+        }
+        
+        
+        
+        
+        
+
 
         // obtain values
-        System.out.println("Enter x position for form 1:");
+        System.out.print("How many forms do you want to show (max 9)? ");
         xPositions[0] = CONSOLE.nextInt();
-        System.out.println("Enter y position for form 1:");
+        System.out.print("How many times do you want the form to move (max 500)?");
         yPositions[0] = CONSOLE.nextInt();
 
         // call methods
@@ -31,18 +63,18 @@ public class PracticeProject3{
 
         // move forms
         for (int i = 0; i < numMoves; i++) {
-            System.out.println("Enter movement number:");
+            System.out.print("Please input the different moves");
             int movement = CONSOLE.nextInt();
 
             // display initial positions
             for (int j = 0; j < numForms; j++) {
-                showForm(panel, forms[j], xPositions[j], yPositions[j], colors[j], 40);
+                showForm(panel, forms[j], xPositions[j], yPositions[j], colors[j], 40); // Size of the R or C is 40
             }
 
             // move and show updated positions
             for (int j = 0; j < numForms; j++) {
-                moveForm(xPositions, yPositions, j, movement);
-                showForm(panel, forms[j], xPositions[j], yPositions[j], colors[j], 40);
+                moveForm(xPositions, yPositions, j, movement); 
+                showForm(panel, forms[j], xPositions[j], yPositions[j], colors[j], 40); // Size of R or C is 40 
             }
 
             // add delay (need to look up for delaying)
@@ -60,10 +92,8 @@ public class PracticeProject3{
         g.setColor(Color.GREEN);
         g.drawString("UTSA - Spring 2023 - CS1083 - Section 001 - Project 3 -", 30, 340);
         g.drawString("SyncedForms - written by Qustandi Fashho", 30, 355);
-        
-
-        
-    }
+        }
+     
 
 
     public static void initialPosition(int[] xPositions, int[] yPositions, int numForms) {
@@ -97,10 +127,10 @@ public class PracticeProject3{
     }
 }
     public static void moveForms(DrawingPanel panel, char[] forms, int[] xPositions, int[] yPositions, Color[] colors, int numForms, int numMoves) {
-    //Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     for (int i = 0; i < numMoves; i++) {
         System.out.println("Enter movement number:");
-        int movement = CONSOLE.nextInt();
+        int movement = scanner.nextInt();
 
         for (int j = 0; j < numForms; j++) {
             showForm(panel, forms[j], xPositions[j], yPositions[j], colors[j], 25);
@@ -112,7 +142,7 @@ public class PracticeProject3{
         }
 
         try {
-            Thread.sleep(50);
+            Thread.sleep(50); // 50 milliseconds 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -158,23 +188,43 @@ public class PracticeProject3{
       yPositions[formIndex] = y;
 }
     public static void showForm(DrawingPanel panel, char form, int xPos, int yPos, Color color, int size) {
-    if (panel == null) {
+      if (panel == null) { 
         System.out.println("Form: " + form + " Position: (" + xPos + ", " + yPos + ") Color: " + color);
-    } else {
+        Graphics g = panel.getGraphics(); ////////////
+        g.setColor(color); //////////
+        g.fillRect(xPos, yPos, size, size); //////////
+        g.setColor(Color.BLACK); /////////   
+    } 
+      else if( form == 'S' || form == 's' || form == 'R' || form == 'r'){
+          Graphics g = panel.getGraphics(); ////////////
+        g.setColor(Color.BLACK); //////////
+        g.drawRect(xPos, yPos, size, size);
+        g.setColor(color); 
+        g.fillRect(xPos, yPos, size, size); //////////
+        g.setColor(Color.BLACK); ///////// 
+        
+      }
+    
+        
+      else {
         Graphics g = panel.getGraphics();
         g.setColor(color);
         g.fillOval(xPos, yPos, size, size);
-        g.setColor(Color.BLACK);
+        g.setColor(Color.BLACK); 
+        /*
+        System.out.print("Type C for circle or R for rectangle"); ////////////////////
+        form = CONSOLE.next().charAt(0); /////////////////////
         if (form == 'R') {
-            g.drawRect(xPos, yPos, size, size / 2);
+            g.drawRect(xPos, yPos, size, size / 2); 
         } else if (form == 'C') {
             g.drawOval(xPos, yPos, size, size);
         } else {
             System.out.println("Invalid form input: " + form);
+           */
         }
     }
 }
-}
+
 
     
 
